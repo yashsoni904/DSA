1410fb713116c4ffa9c15c7182cccbdb97c53b6e
Yash Soni <yashsoni6386@gmail.com>
2025-10-11 22:12:05 +0530

    Time: 0 ms (100%), Space: 42.1 MB (49.32%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count=0;
+        for(int num:nums){
+            if(val!=num){ 
+                nums[count++]=num;
+
+            }
+        }
+        return count;
+    }
+}
