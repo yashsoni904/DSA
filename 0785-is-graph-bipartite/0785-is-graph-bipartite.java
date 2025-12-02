843203bf13422f8839281e6a1b8e3065eb6b1103
Yash Soni <yashsoni6386@gmail.com>
2025-12-02 11:36:07 +0530

    Time: 8 ms (8.12%), Space: 47.3 MB (6.31%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int V=graph.length;
+       List<List<Integer>> adj = new ArrayList<>();
+    
+       for(int i=0;i<V;i++){
+           adj.add(new ArrayList<>());
+           for(int it: graph[i]){
+                 adj.get(i).add(it);
+           }
+       }
+       int[] color = new int[V];
+       for(int i=0;i<V;i++) color[i]=-1;
+       for(int i=0;i<V;i++){
+           if(color[i]==-1){
+               if(bfscheck(i, V ,color ,adj)==false) return false;
+           }
+       }
+        return true;
+    }
+    public boolean bfscheck(int node , int V ,int[] color ,List<List<Integer>> adj ){
+          Queue<Integer> q=new  LinkedList<>();
+          q.offer(node);
+          color[node]=0;
+          while(!q.isEmpty()){
+              int curr = q.poll();
+              for(int it: adj.get(curr)){
+                  if(color[it]==-1){
+                     if(color[curr]==0) color[it]=1;
+                     if(color[curr]==1) color[it]=0;
+                     q.add(it);
+                  }
+                  else{
+                      if(color[curr]==color[it]) return false;
+                  }
+              }
+          }
+          if(q.isEmpty()) return true;
+          return false;
+    }
+}
