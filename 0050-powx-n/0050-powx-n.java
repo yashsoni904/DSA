aeb23923da2a65b75c0d201c21b49f9b5f13d4f4
Yash Soni <yashsoni6386@gmail.com>
2025-12-17 13:49:15 +0530

    Time: 0 ms (100%), Space: 48.2 MB (21.8%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n<0){
+            return 1/power(x,-n);
+        }
+        return power(x,n);
+    }
+    public double power(double x,int n){
+        if(n==0) return 1; 
+         
+        double half= power(x, n/2);
+
+        if(n%2==0) return half*half;
+        else return x*half*half; 
+    }
+}
