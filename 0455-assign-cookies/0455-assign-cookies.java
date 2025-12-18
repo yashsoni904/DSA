eed37cb23992524bf53c42823dafb329643c5704
Yash Soni <yashsoni6386@gmail.com>
2025-12-18 12:26:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int cnt=0;
+        int i=0;
+        int j=0;
+        while(i<g.length && j<s.length){
+            if(g[i++]<=s[j++]) cnt++;
+        }
+        return cnt;
+    }
+}
