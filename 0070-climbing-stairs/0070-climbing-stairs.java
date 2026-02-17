aa6a55a55d09ce6cf756aeac72e26ebfe283a435
Yash Soni <yashsoni6386@gmail.com>
2026-02-17 10:33:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,6 +1,6 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=1) return 1;
-         return climbStairs(n-1)+climbStairs(n-2);
+        if(n==0|| n==1) return 1;
+        return climbStairs(n-1)+climbStairs(n-2);
     }
 }
