da3ecf78b65d76cbba2dd84241593bfc42af2abb
Yash Soni <yashsoni6386@gmail.com>
2026-02-17 11:02:56 +0530

    Time: 0 ms (100%), Space: 42.2 MB (34.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-         if(n<=1) return n;
-       int pre2=0;
-       int pre1=1;
-       for(int i=0;i<n;i++){
-          int curr = pre2+pre1;
-          pre2=pre1;
-          pre1=curr;
-       }
-       return pre1;
+       if(n==0) return 1;
+        if(n==1) return 1;
+        int[] dp =new int[n+1];
+        Arrays.fill(dp , -1);
+        
+        return fibo(n,dp);
+    }
+    public int fibo(int n, int[] dp){
+        if(n==0) return 1;
+        if(n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+
+        return dp[n]=fibo(n-1 , dp)+fibo(n-2,dp);
     }
 }
