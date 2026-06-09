2b41a15a4789cd538c1443666080f9070cb9655a
Yash Soni <yashsoni6386@gmail.com>
2026-06-09 12:54:02 +0530

    Time: 1 ms (40.96%), Space: 43.7 MB (19.57%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -9,28 +9,26 @@ class Node {
 */
 
 class Solution {
-  
     public Node flatten(Node head) {
-        if(head==null ) return null;
-        Node curr = head;
+        Node curr = head; 
         while(curr!=null){
             if(curr.child!=null){
-                Node nex=curr.next;
-                curr.next=flatten(curr.child);
-                curr.next.prev=curr;
-                curr.child=null;
-            
-                while(curr.next!=null){
-                   curr=curr.next;
-                }
-                if(nex!=null){
-                curr.next=nex;
-                nex.prev=curr;
-                }
-            }    
+              Node nex = curr.next;
+              curr.next = flatten(curr.child);
+              curr.next.prev=curr;
+              curr.child=null;
+
+              while(curr.next!=null){
                 curr=curr.next;
+              }
+              if(nex!=null){
+                 curr.next=nex;
+                 nex.prev=curr;
+              }
+
+            }
+            curr=curr.next;
         }
         return head;
-   
     }
 }
