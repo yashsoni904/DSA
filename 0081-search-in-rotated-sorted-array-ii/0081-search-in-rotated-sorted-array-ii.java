af1a40d862a3d83382f3624a8afa8ff5899d9be9
Yash Soni <yashsoni6386@gmail.com>
2026-06-14 13:19:04 +0530

    Time: 0 ms (100%), Space: 45 MB (44.5%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -16,7 +16,7 @@ class Solution {
                else s=mid+1;
             }
             else{
-                if(nums[mid]<target && target<=nums[e]) s=mid+1;
+                if(nums[mid]<=target && target<=nums[e]) s=mid+1;
                 else e=mid-1;
             }
         }
