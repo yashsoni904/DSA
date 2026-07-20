523c462f97a878ad09bd0b107bf7c4ad64f9cef7
Yash Soni <yashsoni6386@gmail.com>
2026-07-20 17:34:55 +0530

    Time: 16 ms (38.91%), Space: 47.6 MB (66.46%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {  //better approach bukket sort
+    public int[] topKFrequent(int[] nums, int k) {  //better approach bucket sort
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
 
-        PriorityQueue<int[]> pq =new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        for(Map.Entry<Integer,Integer> e:map.entrySet()){
-            int num=e.getKey() , f = e.getValue();
-            pq.offer(new int[]{f,num});
-            if(pq.size()>k) pq.poll();
+        PriorityQueue<Integer> pq =new PriorityQueue<>((a,b)->map.get(a)-map.get(b));
+        for(int num : map.keySet()){
+            
+            pq.offer(num);
+            if(pq.size()>k) pq.poll(); // remove the smallest frequency 
         }
 
         int ans[] =new int[k];
         for(int i=0;i<k;i++){
-            ans[i]=pq.poll()[1];
+            ans[i]=pq.poll();
         }
         return ans;
     }
